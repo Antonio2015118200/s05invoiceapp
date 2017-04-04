365a612b04188f749ce7d42ec059a62c72dd91d9
alumno <alumno@LIMSFLAB04-15D.system-uap.adm>
2017-04-03 20:15:56 -0500

    first commit

---
+++ src/s05invoiceapp/S05InvoiceApp.java
@@ -0,0 +1,27 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package s05invoiceapp;
+
+/*
+ *
+ * @author alumno
+ */
+import java.util.Scanner;
+public class S05InvoiceApp {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       System.out.println("Bienvenido a la aplicación Total factura");
+       System.out.println();
+       Scanner sc= new Scanner (System.in);
+       
+       System.out.print("Ingrese subtotal:      ");
+       double subtotal= sc.nextDouble();
+       double impuesto = subtotal*0.18;
+       double total= subtotal + impuesto;      
+    }
+}
